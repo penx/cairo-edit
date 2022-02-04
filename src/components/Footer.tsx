cce2b026128d70f8f99ca7c3b32c4a0738582e72
Alasdair McLeay <alasdair.mcleay@me.com>
2022-02-04 21:29:47 +0000

    About update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,8 @@ import { Paragraph, Link, Text } from '@modulz/design-system';
 export const Footer = () => (
   <>
     <Paragraph>
-      Copyright © 2022{' '}
-      <Link href='https://twitter.com/penx'>Alasdair McLeay</Link>. Based on the
-      work of <Link href='https://twitter.com/susankare'>Susan Kare</Link>.
+      Based on the work of{' '}
+      <Link href='https://twitter.com/susankare'>Susan Kare</Link>.
     </Paragraph>
     <Paragraph>
       Order prints at <Link href='https://kareprints.com'>kareprints.com</Link>.
@@ -16,6 +15,10 @@ export const Footer = () => (
         github.com/penx/cairo-edit
       </Link>
       .
+    </Paragraph>
+    <Paragraph>
+      Copyright © 2022{' '}
+      <Link href='https://twitter.com/penx'>Alasdair McLeay</Link>.
     </Paragraph>
   </>
 );
